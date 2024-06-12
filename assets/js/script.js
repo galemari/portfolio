33924a19f72475913ab35a8afa387ae40a83f6ca
galemari <marianalopezc111@gmail.com>
2024-06-12 00:24:26 -0400

    organizacion de etiquetas y compresion de estilos en orden

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,3 +16,24 @@ document.addEventListener("DOMContentLoaded", function() {
 
   type();
 });
+document.addEventListener("DOMContentLoaded", function() {
+  // Selecciona los íconos y los contenedores de imágenes
+  const frontendIcon = document.getElementById("frontend-icon");
+  const backendIcon = document.getElementById("backend-icon");
+  const frontendImages = document.getElementById("frontend-images");
+  const backendImages = document.getElementById("backend-images");
+
+  // Añade el manejador de eventos de clic para el ícono frontend
+  frontendIcon.addEventListener("click", function() {
+    // Alterna la clase "active" en el contenedor de imágenes frontend
+    frontendImages.classList.toggle("active");
+    backendImages.classList.remove("active"); // Cierra el otro contenedor
+  });
+
+  // Añade el manejador de eventos de clic para el ícono backend
+  backendIcon.addEventListener("click", function() {
+    // Alterna la clase "active" en el contenedor de imágenes backend
+    backendImages.classList.toggle("active");
+    frontendImages.classList.remove("active"); // Cierra el otro contenedor
+  });
+});
